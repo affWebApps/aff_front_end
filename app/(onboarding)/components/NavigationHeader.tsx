ea0b8aeea26cdce0dd7c3fd20d16374ca76f1374
John-Kingsley <117424081+thatkhay@users.noreply.github.com>
2025-11-03 15:40:54 +0100

    fix/NavigationHeader width increase

--- app/(onboarding)/components/NavigationHeader.tsx
+++ app/(onboarding)/components/NavigationHeader.tsx
@@ -17,7 +17,7 @@ export const NavigationHeader: React.FC<NavigationHeaderProps> = ({
     <div className="fixed top-0 md:top-40 lg:top-40 left-0 right-0 z-50">
       <div className="absolute inset-0 bg-[#fff8ef] h-20 md:h-0" />
 
-      <div className="relative max-w-7xl mx-auto py-4 px-4 md:px-8 pt-2">
+      <div className="relative max-w-8xl mx-auto py-4 px-4 md:px-8 pt-2">
         {showSkip && onSkip ? (
           <div className="flex flex-row items-center justify-between">
             <BackButton onClick={onBack} />
